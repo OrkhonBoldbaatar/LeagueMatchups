87da242c9ade6ff3d0423bbc622c42beed9e4c7e
OrkhonBoldbaatar <orkhonboldbaatar@gmail.com>
2026-08-19 11:02:06 +0200

    Verify shared module integration in backend

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import {IS_SHARED_WORKING} from '@league-matchups/shared';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return IS_SHARED_WORKING;
   }
 }
