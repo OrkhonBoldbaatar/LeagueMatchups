ce600324a29a5796d054beab257a13bf56f05e7a
OrkhonBoldbaatar <orkhonboldbaatar@gmail.com>
2026-08-21 14:29:23 +0200

    Created components folder in svelte and moved the button functions

---
+++ frontend/src/lib/components/PostureCheckButton.svelte
@@ -0,0 +1,10 @@
+<script>
+    function pingDiscord() {
+        fetch('http://localhost:3000/posture', {
+            method: 'POST',
+
+        })
+    }
+</script>
+
+<button on:click = {pingDiscord}>Send Posture Check</button>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,13 +1,9 @@
 <script>
     import {IS_SHARED_WORKING} from '@league-matchups/shared';
-    function pingDiscord() {
-        fetch('http://localhost:3000/posture', {
-            method: 'POST',
+    import PostureCheckButton  from "$lib/components/PostureCheckButton.svelte";
 
-        })
-    }
 </script>
 
 <h1>{IS_SHARED_WORKING}</h1>
-<button on:click = {pingDiscord}>Send Posture Check</button>
+<PostureCheckButton></PostureCheckButton>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
